db4322db93ae4d7cae55290e3d59fcd5175ed339
Ned Bellavance <ned.bellavance@gmail.com>
2019-06-23 07:41:19 -0400

    m6 done

---
+++ m6/ProvisionAzureSQLVM.ps1
@@ -0,0 +1,71 @@
+#Prefix for resources
+$prefix = "ced"
+
+#Log into Azure
+Add-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "SUB_NAME" | Select-AzSubscription
+
+#Let's create a SQL VM that we will apply TDE to
+$Location = "eastus"
+$ResourceGroupName = "$prefix-sql-vm"
+$id = Get-Random -Minimum 1000 -Maximum 9999
+$SQLServerName = "$prefix-sql-$id"
+
+$sqlvmRG = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+$templateParameters = @{
+    adminUsername = "winadmin"
+    adminPassword = 'n6Uz^)N.d!j+uE'
+    location = $Location
+    dnsName = "sql$prefix$id"
+    vmName = $SQLServerName
+}
+
+New-AzResourceGroupDeployment -Name "ced-sql-vm" -ResourceGroupName $sqlvmRG.ResourceGroupName -TemplateParameterObject $templateParameters -TemplateFile .\m6\sql-vm.json -Mode Incremental
+
+
+#Now we need to create a Key Vault to use with the SQL VM
+$keyVaultParameters = @{
+    Name = "$prefix-key-vault-$id"
+    ResourceGroupName = $sqlvmRG.ResourceGroupName
+    Location = $location
+    EnabledForDiskEncryption = $true
+    EnabledForDeployment = $true
+    Sku = "Standard"
+}
+$keyVault = New-AzKeyVault @keyVaultParameters
+
+$sqlkey = Add-AzKeyVaultKey -VaultName $keyVault.VaultName -Name "$prefix-sql-key" -Destination 'Software'
+
+#Now create an AAD SPN to use with SQL VM
+Import-Module Az.Resources # Imports the PSADPasswordCredential object
+$credProps = @{
+    StartDate = Get-Date
+    EndDate = (Get-Date -Year 2024)
+    Password = '9MPG7j2MAH3fEveE58vxxg0ghjo9sEutitv9jBeyjfqTLpb9sGBhXQSY9yn2'
+}
+$credentials = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property $credProps
+$sp = New-AzAdServicePrincipal -DisplayName $SQLServerName -PasswordCredential $credentials
+
+#Grant the SPN access to the Key Vault
+$accessPolicy = @{
+    VaultName = $keyVault.VaultName
+    ObjectId = $sp.Id
+    PermissionsToKeys = @("wrapKey","unwrapKey","get","recover")
+}
+Set-AzKeyVaultAccessPolicy @accessPolicy
+
+#Now enable Key Vault integration in the portal using the following values
+#Key Vault URL
+Write-Output $keyVault.VaultUri
+#Principal Name
+Write-Output $sp.ApplicationId.Guid
+#Principal Secret
+Write-Output $credentials.Password
+#Credential name is cedsqlcred
+
+#And update SQL with the proper key info
+Write-Output $sqlkey.Name
+
---
+++ m6/TDE-Enabled.sql
@@ -0,0 +1,41 @@
+CREATE ASYMMETRIC KEY CONTOSO_KEY
+FROM PROVIDER [AzureKeyVault_EKM_Prov]
+WITH PROVIDER_KEY_NAME = 'ced-sql-key',  --The key name here requires the key we created in the key vault
+CREATION_DISPOSITION = OPEN_EXISTING;
+
+USE master;
+-- Create a SQL Server login associated with the asymmetric key
+-- for the Database engine to use when it loads a database
+-- encrypted by TDE.
+CREATE LOGIN EKM_Login
+FROM ASYMMETRIC KEY CONTOSO_KEY;
+GO
+
+-- Alter the TDE Login to add the credential for use by the
+-- Database Engine to access the key vault
+ALTER LOGIN EKM_Login
+ADD CREDENTIAL cedssqlcred;
+GO
+
+-- Create a Contoso Database for TDE  
+CREATE DATABASE [Contoso]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Contoso', FILENAME = N'F:\Data\Contoso.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Contoso_log', FILENAME = N'F:\Log\Contoso_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+GO
+
+--Switch to Contoso Database context
+USE Contoso;
+GO
+
+CREATE DATABASE ENCRYPTION KEY 
+WITH ALGORITHM = AES_128 
+ENCRYPTION BY SERVER ASYMMETRIC KEY CONTOSO_KEY;
+GO
+
+-- Alter the database to enable transparent data encryption.
+ALTER DATABASE Contoso
+SET ENCRYPTION ON;
+GO
